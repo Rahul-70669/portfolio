4b8408244a8dd44eb5da533ba99b635b11a65169
Rahul-70669 <rahulyadav70669@gmail.com>
2025-12-25 14:50:05 +0530

    feat: create textarea atom component

--- src/components/atoms/Textarea/Textarea.tsx
+++ src/components/atoms/Textarea/Textarea.tsx
@@ -4,13 +4,16 @@ interface TextareaProps extends TextareaHTMLAttributes<HTMLTextAreaElement> {
   error?: string;
 }
 
-export const Textarea = forwardRef<HTMLTextAreaElement, TextareaProps>(({ className = '', error, ...props }, ref) => {
-  return (
-    <textarea
-      ref={ref}
-      className={`textarea textarea-bordered w-full ${error ? 'textarea-error' : ''} ${className}`}
-      {...props}
-    />
-  );
-});
+export const Textarea = forwardRef<HTMLTextAreaElement, TextareaProps>(
+  ({ className = '', error, ...props }, ref) => {
+    return (
+      <textarea
+        ref={ref}
+        className={`textarea textarea-bordered w-full ${error ? 'textarea-error' : ''} ${className}`}
+        {...props}
+      />
+    );
+  }
+);
+
 Textarea.displayName = 'Textarea';
